7510ceae67ced836d5a0e744cf2e9cd3305d2759
Greg_PiZero <gregory.birge@wallawalla.edu>
2019-05-09 05:14:28 -0700

    Switch apt-gets to pip3 cmds

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Installing Python Module List"
-sudo apt-get --assume-yes install RPI.GPIO
-sudo apt-get --assume-yes install adafruit-circuitpython-si5351
-sudo apt-get --assume-yes install libffi-dev libffi6
-sudo apt-get --assume-yes install numpy
-sudo apt-get --assume-yes install matplotlib
-sudo apt-get --assume-yes install sounddevice
+sudo yes | pip3 install RPI.GPIO
+sudo yes | pip3 install adafruit-circuitpython-si5351
+sudo yes | pip3 install libffi-dev libffi6
+sudo yes | pip3 install numpy
+sudo yes | pip3 install matplotlib
+sudo yes | pip3 install sounddevice
 
 # Last Update 5/23 - Greg
 
