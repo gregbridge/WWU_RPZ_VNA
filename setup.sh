88506b0c09f1b02a0e7d05d7972c94bfe8ea8909
Greg_PiZero <gregory.birge@wallawalla.edu>
2019-05-09 04:58:13 -0700

    Added setup shell script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Installing Python Module List"
+sudo apt-get --assume-yes install RPI.GPIO
+sudo apt-get --assume-yes install adafruit-circuitpython-si5351
+sudo apt-get --assume-yes install libffi-dev libffi6
+sudo apt-get --assume-yes install numpy
+sudo apt-get --assume-yes install matplotlib
+sudo apt-get --assume-yes install sounddevice
+
+# Last Update 5/23 - Greg
+
+
+
+
+
+
