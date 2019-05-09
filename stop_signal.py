16930b8c221fc365f0f43bb913776b1d52d1224d
Greg_PiZero <gregory.birge@wallawalla.edu>
2019-05-09 15:27:25 -0700

    Added small test commands to simply toggle output signal on and off for testing

---
+++ stop_signal.py
@@ -0,0 +1,8 @@
+import board
+import busio
+import adafruit_si5351
+i2c = busio.I2C(board.SCL, board.SDA)
+si5351 = adafruit_si5351.SI5351(i2c)
+
+si5351.outputs_enabled = False
+print('Output Halted')
