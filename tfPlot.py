c7d9dc94b33bde608e52acd423051c3d4212883f
Greg_PiZero <gregory.birge@wallawalla.edu>
2019-05-10 01:52:24 -0700

    Added time measuring to all over TFplot, copied down error text

--- tfPlot.py
+++ tfPlot.py
@@ -74,7 +74,7 @@ def set_frequency(CLK, freq):
 print('Pre-calculating...')
 # N*T = n * (150 Hz) We will use n = 2
 n = 2
-N = int(n * cfg.fs / cfg.freq_bias )   # n = 2, dividing sample size by 10
+N = int(n * cfg.fs / cfg.freq_bias )
 
 # Building e^(2*np.pi*1j*K*n/N) array for all K points
 K = np.arange(0, N, 1)
@@ -84,7 +84,6 @@ K *= 2*np.pi*1j*n/N
 print('.')
 exp_array = np.exp(-K)
 
-#totalSamples = 48000/10
 
 REF = []
 TST = []
@@ -95,7 +94,7 @@ TST = []
 # CLK 2 - LO_I
 
 F_1 = np.arange(100000, 1000000, 50000)            # 50 kHz -> 1MHz, in 25kHz steps 
-F_2 = np.arange(1000000, 11000000, 100000)       # 1 MHz -> 10 MHz, in 1 MHz steps
+F_2 = np.arange(1000000, 11000000, 1000000)       # 1 MHz -> 10 MHz, in 1 MHz steps
 F_3 = np.arange(10000000, 100000000, 10000000)    # 10 -> 100 MHz, in 10 MHz
 
 
@@ -103,9 +102,11 @@ if(sweep):
     print('Sweeping Frequencies')
     # Second set of Frequencies (Lower frequencies more)
     for i in range(len(F_2)):
+        freq_t = time.clock()
         set_frequency(2, F_2[i] - cfg.freq_bias)
         set_frequency(1, F_2[i])
         set_frequency(0, F_2[i])
+        freq_t2 = time.clock()
 
         print('.')
         # Adding print statements to update user
@@ -114,11 +115,14 @@ if(sweep):
 
         # Enable signal and read signals
         si5351.outputs_enabled = True
-        time.sleep(2)               # Delay to allow Soundcard Cap to charge
-        data = sd.rec(N)
+        time.sleep(1)               # Delay to allow Soundcard Cap to charge
+        start  = time.clock()
+        data = sd.rec(N, blocking = True)
+        
             # data[:,0] is Right channel which is REF
             # data[:,1] is Left channel which is TEST
-        sd.wait()            
+        #sd.wait()
+        stop = time.clock()
         si5351.outputs_enabled = False
 
 
@@ -127,13 +131,15 @@ if(sweep):
 
         REF.append(np.dot(dataR,exp_array)) 
         TST.append(np.dot(dataL,exp_array))
+        stop2 = time.clock()
 
         # These are if the Frequency values are needed
         # REF.append([F_2[i],np.dot(dataR,exp_array)]) 
         # TST.append([F_2[i],np.dot(dataL,exp_array)])
     
-    
-
+print('Time to change freq', freq_t2 - freq_t)    
+print('TIme between SD.rec and sd.wait: ', stop-start)
+print('Time from sd.rec to math', stop2-stop)
 
 # possibly add more loops for other frequency ranges
 
